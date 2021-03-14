9b376f1307786574c64471fb6bbdb73193cd5287
James <jameshooper0820@gmail.com>
2021-03-14 18:14:01 +0000

    mention user on quote

--- bot/main.py
+++ bot/main.py
@@ -8,7 +8,12 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("Logged in as {0.user}.".format(client))
+    print(f'Logged in as {client.user}.')
+
+@client.event
+async def status():
+    activity = discord.Activity(name='Watching your health...', type=discord.ActivityType.watching)
+    await client.change_presence(activity=activity)
 
 @client.event
 async def get_quote():
@@ -19,12 +24,12 @@ async def get_quote():
 
 @client.event
 async def on_message(msg):
-    # Don't do anything if msg is from the bot
     if msg.author == client.user:
         return
     
     if msg.content.startswith('!quote'):
         quote = await get_quote()
-        await msg.channel.send(quote)
+        await msg.channel.send(msg.author.mention + ' ' + quote)
+
 
 client.run(os.getenv('TOKEN'))
