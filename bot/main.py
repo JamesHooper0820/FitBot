91a39eb5fecc1d840695945ab47ab1013fe0638e
James Hooper <jameshooper0820@gmail.com>
2021-05-06 00:29:02 +0100

    added logger, prefix changer and intializer

--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,15 @@
 import discord
+from discord import client
 from discord.ext.commands import Bot
 import os
 import requests
 import json
 import random as r
+from bot.logger import create_logger
 
-bot = Bot(command_prefix='!')
+logger = create_logger(__name__)
+
+bot = Bot(command_prefix="!")
 
 @bot.event
 async def on_ready():
@@ -17,7 +21,7 @@ async def status():
     await bot.change_presence(activity=activity)
 
 @bot.event
-async def get_quote():
+async def get_quote() -> str:
     response = requests.get("https://type.fit/api/quotes")
     json_data = json.loads(response.text)
     random_quote = json_data[r.randint(0, len(json_data))]["text"] + " - " + json_data[r.randint(0, len(json_data))]["author"]
@@ -28,7 +32,24 @@ async def quote(ctx):
     quote = await get_quote()
     await ctx.send(ctx.author.mention + ' ' + quote)
 
-### WORK ON THIS NEXT
+@bot.command(pass_context=True)
+async def intialize(ctx):
+    channel = ctx.message.channel
+    embed = discord.Embed(
+        title = "Title",
+        description = "Description",
+        colour = discord.Color.blue()
+    )
+
+    embed.set_footer(text="Footer")
+    embed.set_thumbnail(url="https://e7.pngegg.com/pngimages/416/261/png-clipart-8-bit-color-8bit-heart-pixel-art-color-depth-allanon-heart-video-game.png")
+    embed.set_author(name="FitBot", icon_url="https://e7.pngegg.com/pngimages/416/261/png-clipart-8-bit-color-8bit-heart-pixel-art-color-depth-allanon-heart-video-game.png")
+    embed.add_field(name="test", value="test value", inline=False)
+    embed.add_field(name="test", value="test value", inline=False)
+
+    await client.send_message(channel, embed=embed)
+
+### WIP
 @bot.command()
 async def posture(ctx):
     user_id_list = [243348545585938433]
@@ -40,7 +61,7 @@ async def posture(ctx):
 async def on_raw_reaction_add(payload):
     message_id = payload.message_id
 
-    if message_id == 821018526357651457: 
+    if message_id == 839555899504590868: 
         guild_id = payload.guild_id 
         guild = discord.utils.find(lambda g : g.id == guild_id, bot.guilds) 
 
@@ -60,7 +81,7 @@ async def on_raw_reaction_add(payload):
 async def on_raw_reaction_remove(payload):
     message_id = payload.message_id
   
-    if message_id == 821018526357651457:
+    if message_id == 839555899504590868:
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g : g.id == guild_id, bot.guilds)
 
---
+++ bot/prefixes.py
@@ -0,0 +1,42 @@
+import json
+from discord.ext.commands import Bot
+
+def get_prefix(client, message) -> str:
+    with open("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    return prefixes[str(message.guild.id)]
+
+bot = Bot(command_prefix = get_prefix)
+
+@bot.event
+async def on_guild_join(guild):
+    with open("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    prefixes[str(guild.id)] = "!"
+
+    with open("prefixes.json", "w") as f:
+        json.dump(prefixes, f, indent=4)
+
+@bot.event
+async def on_guild_remove(guild):
+    with open("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    prefixes.pop(str(guild.id))
+
+    with open("prefixes.json", "w") as f:
+        json.dump(prefixes, f, indent=4)
+
+@bot.command
+async def changeprefix(ctx, prefix):
+    with open("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    prefixes[str(ctx.guild.id)] = prefix
+
+    with open("prefixes.json", "w") as f:
+        json.dump(prefixes, f, indent=4)
+
+    await ctx.send(f"Command prefix changed to: {prefix}")
