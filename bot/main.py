222b89c72039bea179cb965f0d8c429fcf671fba
James Hooper <jameshooper0820@gmail.com>
2021-05-06 19:18:45 +0100

    posture pinging

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext.commands import Bot
+from discord.ext import tasks
 import os
 import requests
 import json
@@ -7,14 +8,18 @@ import random as r
 
 bot = Bot(command_prefix="!")
 
+async def status():
+    activity = discord.Activity(name="your health!", type=discord.ActivityType.watching)
+    await bot.change_presence(activity=activity)
+
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}.")
     await status()
 
-async def status():
-    activity = discord.Activity(name="your health!", type=discord.ActivityType.watching)
-    await bot.change_presence(activity=activity)
+@bot.event
+async def on_guild_join(guild: discord.Guild):
+	await guild.create_role(name="Posture Check", mentionable=True, colour=discord.Colour.blue)
 
 @bot.event
 async def get_quote() -> str:
@@ -23,27 +28,26 @@ async def get_quote() -> str:
     random_quote = json_data[r.randint(0, len(json_data))]["text"] + " - " + json_data[r.randint(0, len(json_data))]["author"]
     return random_quote
 
-@bot.command()
+@bot.command(pass_context=True)
 async def quote(ctx):
     quote = await get_quote()
     await ctx.send(ctx.author.mention + ' ' + quote)
 
-# WIP
-@bot.command()
-async def posture(ctx):
-    user_id_list = [243348545585938433]
-    for user_id in user_id_list:
-        user = await bot.get_user_info(user_id)
-        await user.send(user.mention + "Posture check!")
+@tasks.loop(seconds=60)
+async def posture(guild: discord.Guild, role: discord.Role):
+    role = discord.utils.get(guild.roles, name="Posture Check")
+    if role is None:
+        return
+    for member in guild.members:
+        if role in member.roles:
+            await bot.send_message(member, member.mention + "Hourly posture check, fix your posture!")
 
-# WIP
-@bot.command()
+@bot.command(aliases="createrole")
 async def create_role(ctx, *, name):
 	guild = ctx.guild
 	await guild.create_role(name=name)
 	await ctx.send(f"Role `{name}` has been created.")
 
-# WIP
 @bot.command(pass_context=True)
 async def initialize(ctx):
     embed = discord.Embed(
@@ -52,12 +56,12 @@ async def initialize(ctx):
         colour = discord.Color.blue()
     )
 
-    embed.set_footer(text="Footer")
+    embed.set_footer(text="Stay healthy!")
     embed.set_image(url="https://e7.pngegg.com/pngimages/416/261/png-clipart-8-bit-color-8bit-heart-pixel-art-color-depth-allanon-heart-video-game.png")
     embed.set_thumbnail(url="https://e7.pngegg.com/pngimages/416/261/png-clipart-8-bit-color-8bit-heart-pixel-art-color-depth-allanon-heart-video-game.png")
     embed.set_author(name="FitBot", icon_url="https://e7.pngegg.com/pngimages/416/261/png-clipart-8-bit-color-8bit-heart-pixel-art-color-depth-allanon-heart-video-game.png")
-    embed.add_field(name="test", value="test value", inline=False)
-    embed.add_field(name="test", value="test value", inline=False)
+    embed.add_field(name="Reactions", value="Click on the reactions to this message in order to access roles.", inline=False)
+    embed.add_field(name="🧍", value="Posture Checker Role", inline=False)
 
     initial_message = await ctx.send(embed=embed)
     await initial_message.add_reaction("🧍")
