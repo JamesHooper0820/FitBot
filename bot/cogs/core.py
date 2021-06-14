89f5874159f408ae4d4da6530d88dc2aa32c55ba
JamesHooper0820 <jameshooper0820@gmail.com>
2021-06-14 21:07:35 +0100

    added random workouts

--- bot/cogs/core.py
+++ bot/cogs/core.py
@@ -55,24 +55,48 @@ class Core(commands.Cog):
         quote = await self.get_quote()
         await ctx.send(ctx.author.mention + ' ' + quote)
 
-    # WIP
     @commands.Cog.listener()
-    async def get_workout(self) -> str:
+    async def get_workout(self) -> list:
         response = requests.get("https://wger.de/api/v2/exercise/?language=2")
         json_data = json.loads(response.text)
-        workout = []
-        for i in range(len(json_data["results"])):
-            workout = json_data["results"][i]["name"] + ": " + json_data["results"][i]["description"] + "\n"
+        workouts = []
+        for i in range(5):
+            workouts.append(json_data["results"][r.randint(0, len(json_data["results"])-3)]["name"] + ": " + "\n" + json_data["results"][r.randint(0, len(json_data["results"])-3)]["description"])
             i += 1
+        return workouts
 
-        for r in (("<p>", ""), ("</p>", "")):
-            workout = workout.replace(*r)
-        return workout
-
-    # WIP
     @commands.command(pass_context=True)
     async def workout(self, ctx) -> None:
-        await self.get_workout()
+        workout = await self.get_workout()
+        workouts1 = [w.replace("<p>", "") for w in workout]
+        workouts2 = [w.replace("</p>", "") for w in workouts1]
+        workouts3 = [w.replace("<ul>", "") for w in workouts2]
+        workouts4 = [w.replace("</ul>", "") for w in workouts3]
+        workouts5 = [w.replace("<li>", "") for w in workouts4]
+        workouts6 = [w.replace("</li>", "") for w in workouts5]
+        workouts7 = [w.replace("<ol>", "") for w in workouts6]
+        workouts_final = [w.replace("</ol>", "") for w in workouts7]
+
+        sets = [1, 2, 3]
+        reps = [5, 10, 15]
+
+        embed = discord.Embed(
+            title = "Quick Workout",
+            description = "Below is a list of 5 exercises for you to do, good luck.",
+            colour = discord.Color.blue()
+        )
+
+        embed.set_footer(text="Stay healthy!")
+        embed.set_author(name="FitBot", icon_url="https://e7.pngegg.com/pngimages/416/261/png-clipart-8-bit-color-8bit-heart-pixel-art-color-depth-allanon-heart-video-game.png")
+        embed.add_field(name="Exercises:", value="For exercises that require weights, please use whatever you are comfortable with.", inline=False)
+        embed.add_field(name="\u200b", value="\u200b")
+        embed.add_field(name="Exercise 1 " + "- " + str(r.choice(sets)) + "x" + str(r.choice(reps)) + " reps", value=workouts_final[0], inline=False)
+        embed.add_field(name="Exercise 2 " + "- " + str(r.choice(sets)) + "x" + str(r.choice(reps)) + " reps", value=workouts_final[1], inline=False)
+        embed.add_field(name="Exercise 3 " + "- " + str(r.choice(sets)) + "x" + str(r.choice(reps)) + " reps", value=workouts_final[2], inline=False)
+        embed.add_field(name="Exercise 4 " + "- " + str(r.choice(sets)) + "x" + str(r.choice(reps)) + " reps", value=workouts_final[3], inline=False)
+        embed.add_field(name="Exercise 5 " + "- " + str(r.choice(sets)) + "x" + str(r.choice(reps)) + " reps", value=workouts_final[4], inline=False)
+
+        await ctx.send(embed=embed)
 
     @loop(hours=1)
     async def posture(self) -> None:
