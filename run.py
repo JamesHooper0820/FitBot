5971422e200c67da003f2f37b95ff7336e507b92
JamesHooper0820 <jameshooper0820@gmail.com>
2021-08-07 13:03:03 +0100

    todo update

--- run.py
+++ run.py
@@ -23,7 +23,6 @@ load_dotenv(dotenv_path)
 bot.run(os.environ.get("TOKEN"))
 
 # TO DO:
-# Add 'New Workout' button to workout command, split it up for 1 workout per embed, the edit embed when '>>' button is clicked
 # Discord Buttons & Dropdowns - Improve flow of bot
 # Weekly Calorie Tracker - Tracks last 7 days of calorie inputs, with 7-day average
 # EOD Summary - Total calories and water that day
